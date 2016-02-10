bcbacaf00154149ce67ef93d9e707a719f1eac46
Quinntyne Brown <quinntynebrown@gmail.com>
2016-02-09 19:23:08 -0500

    initial code commit

---
+++ Config/AppConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using System.Configuration;
+
+
+namespace FilePusher.Config
+{
+    public class AppConfiguration: ConfigurationSection
+    {
+        [ConfigurationProperty("projectFolderPath", IsRequired = true)]
+        public string ProjectFolderPath
+        {
+            get { return (string)this["projectFolderPath"]; }
+            set { this["projectFolderPath"] = value; }
+        }
+
+        [ConfigurationProperty("filePath", IsRequired = true)]
+        public string FilePath
+        {
+            get { return (string)this["filePath"]; }
+            set { this["filePath"] = value; }
+        }
+
+        public static AppConfiguration Config
+        {
+            get { return ConfigurationManager.GetSection("appConfiguration") as AppConfiguration; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using FilePusher.Config;
+using System;
+using System.IO;
+
+namespace FilePusher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Leora IO File System Watcher. Press \'q\' to quit.");
+            
+            System.IO.FileSystemWatcher watcher = new System.IO.FileSystemWatcher();
+            watcher.Path = AppConfiguration.Config.ProjectFolderPath;
+            watcher.EnableRaisingEvents = true;
+            watcher.IncludeSubdirectories = true;
+            watcher.Changed += OnChanged;
+
+            string input;
+
+            do
+            {
+                input = Console.ReadLine();
+
+            } while (Console.Read() != 'q');
+        }
+
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            Console.WriteLine("Changed");
+        }
+    }
+}
